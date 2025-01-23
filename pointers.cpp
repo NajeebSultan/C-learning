9ee004aa1dff4d490b240cc057b9c0a36635fbbc
NajeebSultan <najeebsultan070@gmail.com>
2025-01-23 22:55:46 +0500

    Remove somethings

--- pointers.cpp
+++ pointers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Function to take input for the array
 void inputValues(float (&marks)[10]) {
     cout << "Enter marks for 10 students: \n";
     for (int i = 0; i < 10; ++i) {
@@ -10,7 +9,6 @@ void inputValues(float (&marks)[10]) {
     }
 }
 
-// Function to calculate the average of the marks
 void findAverage(float (&marks)[10], float &average) {
     float sum = 0;
     for (int i = 0; i < 10; ++i) {
@@ -20,19 +18,17 @@ void findAverage(float (&marks)[10], float &average) {
 }
 
 int main() {
-    // Declare an array to store marks of 10 students
+    
     float marks[10];
     
-    // Reference to store average
+  
     float average = 0;
 
-    // Call inputValues function to take input
     inputValues(marks);
 
-    // Call findAverage function to calculate the average
+    
     findAverage(marks, average);
 
-    // Display the average
     cout << "The average marks of the 10 students is: " << average << endl;
 
     return 0;
